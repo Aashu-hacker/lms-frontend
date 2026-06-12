b2f79ea4cb8efc2ddf7ab9e2b48f3adf02c46d9a
Aashu-hacker <aashishbopche357@gmail.com>
2026-06-12 19:05:30 +0530

    new updates

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,4 @@
 // const REACT_APP_BASE_URL = "https://backend.eventsgo.in/api";
-// const REACT_APP_BASE_URL = "https://lms-backend-r992.onrender.com/api";
-const REACT_APP_BASE_URL = "http://localhost:5000/api";
+const REACT_APP_BASE_URL = "https://lms-backend-r992.onrender.com/api";
+// const REACT_APP_BASE_URL = "http://localhost:5000/api";
 export default REACT_APP_BASE_URL
